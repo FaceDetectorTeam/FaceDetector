4b2433dc6ffeaa2f687be915daae085f6f29f366
selart <selart66@gmail.com>
2013-09-20 22:25:26 +0400

    commit a new repo
    Face detector gui (initial)

---
+++ FaceDetector/MainWIndow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetector
+{
+    public partial class MainWIndow : Form
+    {
+        public MainWIndow()
+        {
+            InitializeComponent();
+
+            initTabControl();
+        }
+
+        void initTabControl()
+        {
+            tabControl.TabPages.Clear();
+        }
+
+        void addTabPage(string pageName, PictureBox pictureBox)
+        {
+            TabPage tabPage = new TabPage(pageName);
+            tabPage.Controls.Add(pictureBox);
+            tabControl.TabPages.Add(tabPage);
+        }
+       
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Title = "Open...";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Size = new Size(640, 480);
+
+                pictureBox.Image = new Bitmap(openFileDialog.FileName);
+                addTabPage(openFileDialog.FileName, pictureBox);
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Title = "Open...";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Size = new Size(640, 480);
+
+                pictureBox.Image = new Bitmap(openFileDialog.FileName);
+                addTabPage(openFileDialog.FileName, pictureBox);
+
+                
+            }
+        }
+    }
+}
